06bbfd85aada2e56d6f476f988de6eab698832bb
Brandon Belna <bbelna@aol.com>
2026-06-08 11:17:23 -0400

    Setting up deps, cleaning up example code

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,6 +7,4 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('volusia-county-bahais');
-}
+export class App {}
